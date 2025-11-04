366f9f7449cffe6383972b1f057cbfa0005e3b03
Davi Augusto <davi.miranda0919@gmail.com>
2025-11-04 16:54:30 -0300

    iniciando projeto

---
+++ src/main/java/com/daviaugusto/controle_assinaturas/ControleAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.daviaugusto.controle_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daviaugusto/controle_assinaturas/ControleAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daviaugusto.controle_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
